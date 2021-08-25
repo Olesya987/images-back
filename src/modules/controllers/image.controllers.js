59413ff60ce7a99e78b9d2648ce18d017a537879
Olesya987 <oderevianko.exceedteam@gmail.com>
2021-08-25 18:43:30 +0300

    add upload image

--- src/modules/controllers/image.controllers.js
+++ src/modules/controllers/image.controllers.js
@@ -5,10 +5,10 @@ module.exports.postImage = async (req, res) => {
   const { files } = req;
   if (files.hasOwnProperty("img") && files.img) {
     const { img } = files;
-    let fileName = uuid.v4() + ".png";
-    // const way = path.resolve(__dirname, "../..", "source", fileName);
-    const way="/home/user/Desktop/study/images-front/public/images/"+fileName;
-    img.mv(way);
+    let end = img.name.split(".");
+    end = end[end.length - 1];
+    let fileName = uuid.v4() + "." + end;
+    img.mv("/home/user/Desktop/study/images-front/public/images/" + fileName);
     res.send(fileName);
   } else {
     res.status(420).send("Error adding image, image not sent");
